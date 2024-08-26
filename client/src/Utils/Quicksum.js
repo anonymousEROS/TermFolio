50a48eaf6775d00303bc0061a2e224110b8c8760
anonymousEROS <dovala69@gmail.com>
2024-08-26 16:33:13 -0700

    fixed css props

--- client/src/Utils/Quicksum.js
+++ client/src/Utils/Quicksum.js
@@ -1,20 +1,21 @@
 // src/Utils/Quicksum.js
-
 import config from './Config';
+import './quickSum.css'
 
 const Quicksum = async (args) => {
     return `
-                                                     QuickSum
-                                                    -----------
-                                                     ABOUT
-        ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓        ${config.name}
-        ▐▓                                 ▐▓        <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-        ▐▓       > ${config.name}                  ▐▓       -----------
-        ▐▓                                 ▐▓        CONTACT
-        ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-                                                     <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-                                                     <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-
+    <pre>
+ QuickSum
+-----------
+ ABOUT
+ ${config.name}
+ <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+-----------
+ CONTACT
+ <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+ <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+ <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    </pre>
     `;
 };
 
